092bb9b1e8abd55ff95e09600046c8e9466c4797
Shawn Schreier <shawn.schreier13@gmail.com>
2025-07-07 02:54:57 +0000

    Added test main file to test API calls

---
+++ test_main.py
@@ -0,0 +1,19 @@
+from fastapi.testclient import TestClient
+from main import app
+
+client = TestClient(app)
+
+def test_read_main():
+    response = client.get("/")
+    assert response.status_code ==200
+    assert response.json() == {"message":"API health check successful"}
+
+def test_read_players():
+    response = client.get("/v0/players/?savant_id=592450")
+    assert response.status_code==200
+    assert response.json()[0].get("savant_id")==592450
+
+def test_counts():
+    response = client.get("/v0/counts")
+    assert response.status_code==200
+    assert response_data["player_count"]>0
