cd26968b393b78fd78b5dd73197cf62ec0cbbc5a
Shawn Schreier <shawn.schreier13@gmail.com>
2025-07-09 17:38:16 +0000

    Tweak test_crud

--- test_crud.py
+++ test_crud.py
@@ -13,7 +13,6 @@ def db_session():
 def test_get_player(db_session):
     """Tests that you can return Aaron Judge"""
     player = crud.get_player(db_session, savant_id=592450)
-    print(player.savant_id)
     #assert player.savant_id==592450
 
 def test_get_logs(db_session):
