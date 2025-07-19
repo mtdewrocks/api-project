3b6e6573d99c2f38701caf45199dcf45fec47b47
Shawn Schreier <shawn.schreier13@gmail.com>
2025-07-19 14:52:43 -0500

    Updating hitters files

---
+++ api/add_hitters.sh
@@ -0,0 +1,8 @@
+git add .
+
+git commit -m "Updating hitters files"
+
+git push "https://github.com/mtdewrocks/api-project/tree/main/api"
+
+
+exit
---
+++ api/hitters_push.bat
@@ -0,0 +1,2 @@
+cd /D C:\Users\shawn\Python\api\api
+"C:\Program Files\Git\bin\bash.exe" -c "./add_hitters.sh"
