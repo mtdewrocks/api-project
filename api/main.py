7c42f3f1cd6c70ce2ad9f99f43ad22edbc16ceae
Shawn Schreier <shawn.schreier13@gmail.com>
2025-07-29 00:16:35 +0000

    fix game logs argument name

--- api/main.py
+++ api/main.py
@@ -36,7 +36,7 @@ def read_pitcher(savant_id:int=None,fg_id:int=None,baseball_reference_name:str=N
 
 @app.get("/v0/game_logs/", response_model=list[schemas.Logs])
 def get_logs(name: str=None, fg_id:int=None, savant_id:int=None, date: str=None, tm: str=None, db: Session=Depends(get_db)):
-    logs = crud.get_logs(db, fg_id=fd_id, savant_id=savant_id, name=name, date=date, tm=tm).all()
+    logs = crud.get_logs(db, fg_id=fg_id, savant_id=savant_id, name=name, date=date, tm=tm).all()
     return logs
     
 @app.get("/v0/counts/", response_model=schemas.Counts)
